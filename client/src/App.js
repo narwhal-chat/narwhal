e7fb9ff64cc0c218a7e33eb1d514999231fc43a3
Rory Well <rory.e.well@gmail.com>
2018-03-06 20:35:18 -0600

    Add ChatView component to house conditional main chat view logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+
 import './App.css';
-import Auth from './containers/Auth/Auth'
+import Auth from './components/Auth/Auth';
+import ChatView from './components/Chat/ChatView/ChatView';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Auth />
-      </div>
+      <React.Fragment>
+        {/* <Auth /> */}
+        <ChatView />
+      </React.Fragment>
     );
   }
 }
---
+++ client/src/components/Chat/ChatView/PodContainer/PodContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './PodContainer.css';
+
+class PodContainer extends Component {
+  render () {
+    return (
+      <div className="PodContainer">
+      </div>
+    );
+  }
+}
+
+export default PodContainer;
