6186c6b7799756b99eca5d9052b999f6e29acf0e
Rory Well <rory.e.well@gmail.com>
2018-03-07 19:53:09 -0600

    Add logo and DISCOVER text to the PodContainer

--- client/src/components/Chat/ChatView/PodContainer/PodContainer.js
+++ client/src/components/Chat/ChatView/PodContainer/PodContainer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import './PodContainer.css';
+import narwhalLogo from '../../../../assets/images/narwhal.png';
 
 class PodContainer extends Component {
   render () {
     return (
       <div className="PodContainer">
+        <img className="PodContainer-logo" src={narwhalLogo} />
+        <div className="PodContainer-discover">DISCOVER</div>
+        <div className="PodContainer-discover-line"></div>
       </div>
     );
   }
