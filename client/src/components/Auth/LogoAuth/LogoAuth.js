48796ade45f59cd7acbac67396cfea4b6740fc1b
Rory Well <rory.e.well@gmail.com>
2018-03-31 20:43:39 -0500

    Update narwhal logo with an SVG for HD goodness and remove old images

--- client/src/components/Auth/LogoAuth/LogoAuth.js
+++ client/src/components/Auth/LogoAuth/LogoAuth.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './LogoAuth.css';
-import logo from './logo.png'
+import narwhalLogo from '../../../assets/images/narwhal.svg';
 
 const LogoAuth = props => (
 
         <div className={styles.LogoContainer}>
-            <img className={styles.LogoImageLrg} src={logo} alt="Logo" />
+            <img className={styles.LogoImageLrg} src={narwhalLogo} alt="Logo" />
             <div className={styles.AuthText}>
                 <p className={styles.LogoText}>narwhal</p>
                 <p>get to the point</p>
