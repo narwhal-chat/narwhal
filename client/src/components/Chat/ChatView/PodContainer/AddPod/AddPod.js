c232802acfb6fecd3b152dae13f63c9c376b60e0
Rory Well <rory.e.well@gmail.com>
2018-03-31 21:58:17 -0500

    Make CSS updates to the add pod icon and the pod header in the topics container

--- client/src/components/Chat/ChatView/PodContainer/AddPod/AddPod.js
+++ client/src/components/Chat/ChatView/PodContainer/AddPod/AddPod.js
@@ -6,7 +6,7 @@ import styles from './AddPod.css';
 const addPod = (props) => {
   return (
     <div onClick={props.openModal} className={styles.AddPod}>
-      <PlusIcon size="68%" />
+      <PlusIcon size="80%" />
     </div>
   );
 };
