22c1185987950df4ac47dbe1b695262205237fe0
sleex89 <samlee723@gmail.com>
2018-04-03 14:02:28 -0700

    Added styling for active category

--- client/src/components/Chat/ChatView/DiscoverCategoriesContainer/DiscoverCategories/DiscoverCategory/DiscoverCategory.js
+++ client/src/components/Chat/ChatView/DiscoverCategoriesContainer/DiscoverCategories/DiscoverCategory/DiscoverCategory.js
@@ -3,9 +3,16 @@ import React from 'react';
 import styles from './DiscoverCategory.css';
 
 const DiscoverCategory = props => {
+  let categoryName = null;
+  if (props.activeCategory === props.category.name) {
+    categoryName = [styles.Category, styles.Active].join(' ');
+  } else {
+    categoryName = styles.Category;
+  }
+
 	return(
     <div 
-      className={styles.Category}>
+      className={categoryName}>
       <div className={styles.CategoryName} value={props.category.name} onClick={() => props.categoryClick(props.category.name)}># {props.category.name}</div>
     </div>
   )
