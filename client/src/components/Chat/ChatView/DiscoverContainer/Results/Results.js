e213f9088556fad6aea4abca51d553d0b778b0a4
sleex89 <samlee723@gmail.com>
2018-04-05 17:20:49 -0700

    sorting discover categories by user count

--- client/src/components/Chat/ChatView/DiscoverContainer/Results/Results.js
+++ client/src/components/Chat/ChatView/DiscoverContainer/Results/Results.js
@@ -10,7 +10,7 @@ class Results extends Component {
 		let results = null;
 
 		if (this.props.activeCategory === null) {
-			results = this.props.results.map(result => {
+			results = this.props.results.sort((a, b) => b.user_count - a.user_count).map(result => {
 				return (
 					<Result key={result.id} currentPods={this.props.currentPods} result={result} joinPod={this.props.joinPod}/>
 				)
@@ -18,7 +18,7 @@ class Results extends Component {
 		} else {
 			results = this.props.results.filter(filterResults => {
 				return filterResults.pod_category_name === this.props.activeCategory
-			}).map(result => {
+			}).sort((a, b) => b.user_count - a.user_count).map(result => {
 				return (
 					<Result key={result.id} currentPods={this.props.currentPods} result={result} joinPod={this.props.joinPod}/>
 				)
