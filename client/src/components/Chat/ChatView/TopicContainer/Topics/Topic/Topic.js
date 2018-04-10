aa7541295d7e311471d4299132bdfe7bb7d38298
Rory Well <rory.e.well@gmail.com>
2018-04-10 00:10:21 -0500

    Make changes to the truncation length on pod header and the topic title

--- client/src/components/Chat/ChatView/TopicContainer/Topics/Topic/Topic.js
+++ client/src/components/Chat/ChatView/TopicContainer/Topics/Topic/Topic.js
@@ -15,7 +15,7 @@ const topic = (props) => {
     <div
       className={topicStyle}
       onClick={() => props.clicked(props.topic)}>
-      <div className={styles.TopicName}># {props.topic.name.length >= 19 ? props.topic.name.substring(0, 19) + '...' : props.topic.name}</div>
+      <div className={styles.TopicName}># {props.topic.name.length > 19 ? props.topic.name.substring(0, 19) + '...' : props.topic.name}</div>
     </div>
   );
 };
