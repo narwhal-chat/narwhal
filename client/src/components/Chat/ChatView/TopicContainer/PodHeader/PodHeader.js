1d8628aa75976bb6ae7be897d049be80e7c0380e
Rory Well <rory.e.well@gmail.com>
2018-04-09 23:58:14 -0500

    Truncate topic names and pod header names that are longer than a certain length to prevent new line

--- client/src/components/Chat/ChatView/TopicContainer/PodHeader/PodHeader.js
+++ client/src/components/Chat/ChatView/TopicContainer/PodHeader/PodHeader.js
@@ -4,7 +4,7 @@ import styles from './PodHeader.css';
 
 const podHeader = (props) => {
   return (
-    <div className={styles.PodHeader}>{props.name}</div>
+    <div className={styles.PodHeader}>{props.name.length >= 10 ? props.name.substring(0, 14) + '...' : props.name}</div>
   );
 };
 
