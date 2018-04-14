6e0e836aaf1a68a84a4e19e8d6ca6b1b746f56cd
Rory Well <rory.e.well@gmail.com>
2018-04-13 20:30:44 -0500

    Remove search button on the discover page

--- client/src/components/Chat/ChatView/DiscoverContainer/DiscoverSearch/DiscoverSearch.js
+++ client/src/components/Chat/ChatView/DiscoverContainer/DiscoverSearch/DiscoverSearch.js
@@ -7,16 +7,12 @@ import * as actions from '../../../../../store/actions/index';
 
 class DiscoverSearch extends Component {
   render() {
-    
     return(
       <div className={styles.DiscoverSearch}>
-        <SearchInput className={styles.SearchBar} onChange={this.props.changeSearch} placeholder="Enter a pod, reference name or description"/>
-        <button className={styles.Button}>Search</button>
+        <SearchInput className={styles.SearchBar} onChange={this.props.changeSearch} placeholder="Find new pods"/>
       </div>
     )
   }
 }
-
-
 
 export default DiscoverSearch;
