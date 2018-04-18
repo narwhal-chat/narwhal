0062449824207d91f5e57fc5d3013385b036919f
Rory Well <rory.e.well@gmail.com>
2018-04-18 00:04:42 -0500

    Dynamically populate all the message search result content, including avatar, date, username, and message

--- client/src/components/Chat/ChatView/MessageContainer/MessageSearchContainer/MessageSearchResults/MessageSearchResult/MessageSearchResult.js
+++ client/src/components/Chat/ChatView/MessageContainer/MessageSearchContainer/MessageSearchResults/MessageSearchResult/MessageSearchResult.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import moment from 'moment';
 
 import styles from './MessageSearchResult.css';
 
 const messageSearchResult = (props) => {
+  console.log(props.messageSearchResult);
+  let avatar = {
+    backgroundImage: `url('${props.messageSearchResult.avatar}')`
+  }
+
   return (
-    <div className={styles.MessageSearchResult}>{props.messageSearchResult.message_text}</div>
+    <div className={styles.MessageSearchResult}>
+      <div
+        className={styles.Avatar}
+        style={avatar}
+        draggable="false">
+      </div>
+      <div className={styles.MessageContent}>
+        <div className={styles.Username}>
+          {props.messageSearchResult.username}
+        </div>
+        <div className={styles.Time}>
+          {moment(props.messageSearchResult.create_date).calendar()}
+        </div>
+        <div className={styles.MessageText}>
+          {props.messageSearchResult.message_text}
+        </div>
+      </div>
+      <div className={styles.MessageSeparator}></div>
+    </div>
   );
 };
 
